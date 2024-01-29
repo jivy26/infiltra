4515bd686160edb13e3e8eaea8ad3655c5db4cae
jivy26 <jivy26@gmail.com>
2024-01-28 18:25:20 -0600

    Rebranded, Simplified Installation, Improved Functionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='infiltra',
-    version='2.1',
+    version='2.0',
     packages=find_packages(),
     install_requires=[
         'colorama',
