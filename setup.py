7a690a8e10af97ce7538e05da4a70d04d8545bd6
jivy26 <jivy26@gmail.com>
2024-02-13 18:25:30 -0600

    Removed in-app update function, added update checker to menu.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='infiltra',
-    version='2.7',
+    version='2.8',
     packages=find_packages(),
     install_requires=[
         'colorama',
