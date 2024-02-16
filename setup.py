0dbe2552fb69a49357a877d3d377cfb97cc55652
jivy26 <jivy26@gmail.com>
2024-02-16 07:29:33 -0600

    Modularized, Fixed Logic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='infiltra',
-    version='3.03',
+    version='3.3',
     packages=find_packages(),
     install_requires=[
         'colorama',
