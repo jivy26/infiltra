0558700947803e8a98c14b09bebb7a1be8dbc0b7
jivy26 <jivy26@gmail.com>
2024-02-15 21:05:17 -0600

    Moved from qterminal to gnome, added website enumeration submenu

--- infiltra/submenus/osint_sub.py
+++ infiltra/submenus/osint_sub.py
@@ -177,7 +177,8 @@ def osint_submenu(project_path):
 
     while True:
         clear_screen()
-        print(f"{BOLD_CYAN}OSINT Menu: {BOLD_GREEN}Domain: {domain}\n")
+        print(f"{BOLD_CYAN}OSINT Menu:")
+        print(f"{BOLD_CYAN}Domain: {BOLD_GREEN}{domain}\n")
         menu_options = [
             ("1. Set Domain", f"{DEFAULT_COLOR}Define the domain to be used for OSINT."),
             ("2. Run AORT and DNSRecon", f"{DEFAULT_COLOR}Enumerate DNS information."),
