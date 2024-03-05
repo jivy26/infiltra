809c55211aa6034197d3a558e21fca186769727d
jivy26 <jivy26@gmail.com>
2024-03-05 10:40:21 -0600

    Error Handling Improvements, Renamed from ept.py

--- infiltra/submenus/osint_sub.py
+++ infiltra/submenus/osint_sub.py
@@ -25,7 +25,7 @@ BOLD_WHITE = Fore.WHITE + Style.BRIGHT
 def run_eyewitness(domain):
     clear_screen()
     script_directory = os.path.dirname(os.path.realpath(__file__))
-    eyewitness_dir = os.path.join(script_directory, 'eyewitness')
+    eyewitness_dir = os.path.join(script_directory, '..', 'eyewitness')
     eyewitness_script_path = os.path.join(eyewitness_dir, 'eyewitness.py')
 
     # Set default file path
