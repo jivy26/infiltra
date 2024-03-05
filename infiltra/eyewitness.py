c552e563a6c6c480bd927c828ae46e683161568f
jivy26 <jivy26@gmail.com>
2024-03-05 11:36:28 -0600

    Error Handling Improvements, Renamed from ept.py

--- infiltra/eyewitness.py
+++ infiltra/eyewitness.py
@@ -1,11 +1,35 @@
 import os
 import subprocess
+import re
 
 from infiltra.utils import clear_screen, BOLD_GREEN, BOLD_CYAN
 
+
+# Regex for validating a domain
+domain_regex = re.compile(
+    r'^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$'
+)
+
+def is_valid_domain(domain):
+    return re.match(domain_regex, domain) is not None
+
+def enumerate_and_screenshot_domain(domain):
+    rand = os.urandom(4).hex()  # Using random hex instead of RANDOM for better uniqueness
+    temp_output_file = f"/tmp/enum_tmp_{rand}.txt"
+    crtsh_command = (
+        f"curl -fsSL 'https://crt.sh/?q={domain}' | pup 'td text{{}}' | "
+        f"grep '{domain}' | sort -n | uniq | httprobe > {temp_output_file}"
+    )
+    subprocess.run(crtsh_command, shell=True, check=True)
+    eyewitness_command = ['eyewitness', '-f', temp_output_file, '--web']
+    subprocess.run(eyewitness_command, check=True)
+    os.remove(temp_output_file)  # Clean up the temp file
+
+def run_eyewitness(input_path):
+    subprocess.run(['eyewitness', '-f', input_path, '--web'], check=True)
+
 def main():
     clear_screen()
-
     # Set default file path
     default_file = 'aort_dns.txt'
 
@@ -17,16 +41,19 @@ def main():
         f"\n{BOLD_GREEN}Enter a single IP, domain, or path to a file with domains (leave blank to use default aort_dns.txt from nmap_grep): "
     ).strip()
 
-    # Determine which file or IP to use
-    input_file = user_input if user_input else default_file
+    # If user input is a valid domain, perform enumeration and screenshot process
+    if is_valid_domain(user_input):
+        enumerate_and_screenshot_domain(user_input)
+    # If user input is a path to a file or left blank, use the normal EyeWitness process
+    elif os.path.isfile(user_input) or not user_input:
+        run_eyewitness(user_input or default_file)
+    else:
+        print(f"{BOLD_GREEN}Invalid input. Please enter a valid domain or file path.")
 
-    # Inform the user which input will be used
-    print(f"Using file: {input_file}" if os.path.isfile(input_file) else f"Using IP/domain: {input_file}")
+    input(f"{BOLD_GREEN}Press Enter to return to the menu...")
 
-    # Run the EyeWitness system command
-    subprocess.run(['eyewitness', '-f', input_file, '--web'])  # Modify the arguments as needed for EyeWitness
-
-    input(f"{BOLD_GREEN}Press Enter to return to the menu...")  # Allow users to see the message before returning to the menu
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
