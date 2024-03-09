354edb8e265ccd600cca50f2cd24aa65eb19f982
jivy26 <jivy26@gmail.com>
2024-03-09 09:28:49 -0600

    Fixed sslscan error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='infiltra',
-    version='3.5',
+    version='3.6',
     packages=find_packages(),
     install_requires=[
         'colorama',
