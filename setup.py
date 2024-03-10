29a6fb551860409bf214f42acd09183ad83d8a22
jivy26 <jivy26@gmail.com>
2024-03-10 11:54:21 -0500

    Incorporated SIPPTS

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         'pydig',
         'ascii_magic',
         'pyfiglet',
-        'pipx'
+        'pipx',
+        'pymetasploit3'
     ],
     entry_points={
         'console_scripts': [
