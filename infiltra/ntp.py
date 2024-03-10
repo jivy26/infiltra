04de0412e2a4f915bf9f51290f1c17a483eaa879
jivy26 <jivy26@gmail.com>
2024-03-10 11:36:27 -0500

    Incorporated SIPPTS

--- infiltra/ntp.py
+++ infiltra/ntp.py
@@ -4,20 +4,27 @@ import os
 from infiltra.utils import RICH_CYAN, RICH_RED, RICH_GREEN, console, clear_screen
 
 def run_ntpq(hosts, output_dir):
-    clear_screen()
     output_file = os.path.join(output_dir, 'ntpq.txt')
     with open(output_file, 'w') as file:
         for host in hosts:
+            print(f"Running ntpq -p on {host}")
             try:
-                console.print(f"Running ntpq -p on {host}", style=RICH_CYAN)
                 result = subprocess.run(['ntpq', '-p', host], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-                output = f"Results for {host}:\n{result.stdout}\n\n"
-                print(output)  # Print to the console
-                file.write(output)  # Write to the file
+                if "**Request timed out" in result.stderr:
+                    # Handle timeout specifically if stderr contains the timeout message
+                    timeout_msg = f"ntpq request timed out for {host}\n\n"
+                    print(timeout_msg)  # Print to the console
+                    file.write(timeout_msg)  # Write to the file
+                else:
+                    # If no timeout, write the standard output to file
+                    output = f"Results for {host}:\n{result.stdout}\n\n"
+                    print(output)  # Print to the console
+                    file.write(output)  # Write to the file
             except subprocess.CalledProcessError as e:
                 error_msg = f"Failed to execute ntpq for {host}: {e}\n\n"
                 print(error_msg)  # Print to the console
                 file.write(error_msg)  # Write to the file
+
 
 def run_ntp_fuzzer(hosts, output_dir):
     output_file = os.path.join(output_dir, 'ntp_fuzzer.txt')
