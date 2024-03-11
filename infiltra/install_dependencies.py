a7c04ab07425970293bed8bd6b7ac692535948af
jivy26 <jivy26@gmail.com>
2024-03-11 12:08:56 -0500

    Incorporated SIPPTS

--- infiltra/install_dependencies.py
+++ infiltra/install_dependencies.py
@@ -40,18 +40,20 @@ def check_and_install_eyewitness():
 
 def check_and_install_sippts():
     try:
+        # Check if 'sipscan' command is available in the system path
         subprocess.run(["which", "sipscan"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(f"{BOLD_GREEN}sippts is installed.")
     except subprocess.CalledProcessError:
-        tmp_dir = '/tmp/sippts'  # Use /tmp as the directory for installation
         print(f"{BOLD_YELLOW}sippts is not installed. Installing now...")
         try:
+            # Use /tmp as the directory for installation to avoid permission issues
+            tmp_dir = '/tmp/sippts'
             # Ensure the directory is clean before cloning
             subprocess.run(["rm", "-rf", tmp_dir], check=True)
+            # Clone the repository
             subprocess.run(["git", "clone", "https://github.com/Pepelux/sippts.git", tmp_dir], check=True)
-            # Install using pip
+            # Install using pip in editable mode
             subprocess.run(["pip", "install", "-e", tmp_dir], check=True)
-            # The cloned repository is left in /tmp and will eventually be cleaned up by the system
             print(f"{BOLD_GREEN}sippts installed successfully.")
         except subprocess.CalledProcessError as e:
             print(f"{BOLD_RED}Failed to install sippts: {e}")
