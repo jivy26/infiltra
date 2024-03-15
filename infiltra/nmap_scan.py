ece62e83cfd5f2b0ae6f3f6b4c5bda5c3d3029d4
jivy26 <jivy26@gmail.com>
2024-03-15 08:21:47 -0500

    Added screenshotting

--- infiltra/nmap_scan.py
+++ infiltra/nmap_scan.py
@@ -3,7 +3,6 @@ import subprocess
 import ipaddress
 import sys
 
-
 def is_valid_ip(ip):
     try:
         ipaddress.ip_address(ip)
@@ -11,19 +10,10 @@ def is_valid_ip(ip):
     except ValueError:
         return False
 
-
 def nmap_is_installed():
     return subprocess.run(['which', 'nmap'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).returncode == 0
 
-
-def run_nmap_scan(ip_list, scan_type, project_path, schedule=False):
-    # Create the 'tmp' directory within the project path if it doesn't exist
-    tmp_dir = os.path.join(project_path, 'tmp')
-    os.makedirs(tmp_dir, exist_ok=True)
-
-    # Define the path to the marker file inside the 'tmp' direc
-    marker_file = os.path.join(tmp_dir, "nmap_scan_ongoing.marker")
-
+def run_nmap_scan(ip_list, scan_type):
     if scan_type not in ["tcp", "udp", "both"]:
         print("Invalid scan type: Choose 'tcp', 'udp', or 'both'.")
         sys.exit(1)
@@ -36,40 +26,22 @@ def run_nmap_scan(ip_list, scan_type, project_path, schedule=False):
     tcp_scan_command = f"sudo nmap -sSV --top-ports 4000 -Pn -oG tcp.txt {' '.join(ip_list)}"
     udp_scan_command = f"sudo nmap -sU --top-ports 400 -Pn -oG udp.txt {' '.join(ip_list)}"
 
-    if not schedule:
-        # For immediate execution, use gnome-terminal
-        if scan_type == "tcp" or scan_type == "both":
-            tcp_command = ['gnome-terminal', '--', 'bash', '-c',
-                           f"{tcp_scan_command} || echo 'An error occurred with the TCP scan.'; echo 'Press enter to exit'; read"]
-            subprocess.Popen(tcp_command)
+    # For immediate execution, use gnome-terminal
+    if scan_type == "tcp" or scan_type == "both":
+        tcp_command = ['gnome-terminal', '--', 'bash', '-c',
+                       f"{tcp_scan_command} || echo 'An error occurred with the TCP scan.'; read"]
+        subprocess.Popen(tcp_command)
 
-        if scan_type == "udp" or scan_type == "both":
-            udp_command = ['gnome-terminal', '--', 'bash', '-c',
-                           f"{udp_scan_command} || echo 'An error occurred with the UDP scan.'; echo 'Press enter to exit'; read"]
-            subprocess.Popen(udp_command)
-    else:
-        # Create a marker file to indicate the scan has started
-        with open(marker_file, "w") as f:
-            f.write("Scan started")
-
-        # For scheduled execution, run the command directly
-        if scan_type == "tcp" or scan_type == "both":
-            subprocess.run(tcp_scan_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        if scan_type == "udp" or scan_type == "both":
-            subprocess.run(udp_scan_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-       # Delete the marker file
-        if os.path.exists(marker_file):
-            os.remove(marker_file)
-
+    if scan_type == "udp" or scan_type == "both":
+        udp_command = ['gnome-terminal', '--', 'bash', '-c',
+                       f"{udp_scan_command} || echo 'An error occurred with the UDP scan.'; read"]
+        subprocess.Popen(udp_command)
 
 def main():
     if len(sys.argv) < 3:
-        print("Usage: nmap_scan.py [single_ip/ip_file_path] [tcp/udp/both] [True/False for scheduled]")
+        print("Usage: nmap_scan.py [single_ip/ip_file_path] [tcp/udp/both]")
         sys.exit(1)
 
-    scheduled = len(sys.argv) == 4 and sys.argv[3] == 'True'
     input_arg = sys.argv[1]
     scan_type = sys.argv[2].lower()
 
@@ -77,16 +49,12 @@ def main():
         if os.path.isfile(input_arg):
             with open(input_arg, 'r') as file:
                 ip_list = file.read().splitlines()
-                # Validate IPs in file
-                for ip in ip_list:
-                    if not is_valid_ip(ip):
-                        print(f"Invalid IP found in file: {ip}")
-                        sys.exit(1)
+                ip_list = [ip for ip in ip_list if is_valid_ip(ip)]
             # Run scan with file input
-            run_nmap_scan(["-iL", input_arg], scan_type, scheduled)
+            run_nmap_scan(["-iL", input_arg], scan_type)
         elif is_valid_ip(input_arg):
             # Run scan with single IP
-            run_nmap_scan([input_arg], scan_type, scheduled)
+            run_nmap_scan([input_arg], scan_type)
         else:
             print("Invalid input: Please provide a valid single IP or a path to a text file with IPs.")
             sys.exit(1)
@@ -94,7 +62,6 @@ def main():
     except Exception as e:
         print(f"An error occurred: {e}")
         sys.exit(1)
-
 
 if __name__ == "__main__":
     main()
