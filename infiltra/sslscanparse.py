f6435bb7a925c94ea9b4afccd218fd79686c1213
jivy26 <jivy26@gmail.com>
2024-06-04 05:46:57 -0500

    Updated nmap-grep, improved last project, added screenshot functionality.

--- infiltra/sslscanparse.py
+++ infiltra/sslscanparse.py
@@ -228,3 +228,4 @@ with open(ip_file_path, 'r') as file, open('sslscan.txt', 'w') as output_file:
                 output_file.write(f"\nNo findings for {ip}:{port}, automatically loading a window to run scans for a screenshot.\n")
                 print(f"\n{YELLOW}No findings for {ip}:{port}, automatically loading a window to run scans for a screenshot.{END}", flush=True)
                 open_new_terminal_and_run_sslscan(ip, port)
+#1
